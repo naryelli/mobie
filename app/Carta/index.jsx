fda0b1d547acba2216b53017e3103e04cd15354c
NARYELLI JULIA DE SOUZA HENRIQUE <naryelli_henrique@estudante.sesisenai.org.br>
2024-09-09 12:00:32 -0300

    sla

--- app/Carta/index.jsx
+++ app/Carta/index.jsx
@@ -1,47 +1,72 @@
-import React, {useState, useEffect} from "react"; 
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
     container: {
-        justifyContent:'center',
-        alignItems:'center',
-        flex:'1', 
-      
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: '1',
     },
+    title: {
+        fontSize: 30,
+        textAlign: 'center',
+        margin: 20,
+        fontFamily: 'Open Sans',
+        fontWeight: '800',
+    },
+    box:{
+        shadowOpacity: 0.25,  
+        shadowRadius: 8.84, 
+       padding: 50
+    },
+    picker:{
+      borderRadius:15,
+      color:'gray',
+    },
+    final:{
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 20,
+        fontFamily: 'Open Sans',
+        fontWeight: '800',   
+    }
+
 });
 
 export default Seletor = () => {
     const [pokemon, setPokemon] = useState('');
-    const[lista_pokemon, setListaPokemon] = useState ([])
+    const [lista_pokemon, setListaPokemon] = useState([])
 
     ///const lista_pokemon = [
-       // {nome:'Pikachu', value: 'pikachu'},
-        //{nome:'Bulbasaur', value: 'Bulbasaur'},
-       // {nome:'Charmander', value: 'charmander'},
-       // {nome:'Squirtle', value: 'squirtle'}
-  // ];
-   useEffect (()=> {
-    fetch (' https://pokeapi.co/api/v2/pokemon?limit=100')
-        .then(response =>response.json ())
-        .then(dados => setListaPokemon (dados.results))
-   }, []) 
-   console.log ('fora')
-   
-   return(
-        <View style = {styles.container}>
-               <Text style={styles.title}>Selecione um Pokemon</Text>
-               <Picker
-                  selectedValue={pokemon}
-                  style={styles.picker}
-                  onValueChange={(itemValue) => setPokemon(itemValue)}
-               >
-                     <Picker.Item label = "Selecione um Pokemon"/>
-                     {lista_pokemon.map ((item, index)=>(
-                        <Picker.Item key={index} label={item.name} value={item.url}/>
-                     ))}
-               </Picker>
-               {pokemon? <Text>Voce selecionou {pokemon}</Text>:''}
+    // {nome:'Pikachu', value: 'pikachu'},
+    //{nome:'Bulbasaur', value: 'Bulbasaur'},
+    // {nome:'Charmander', value: 'charmander'},
+    // {nome:'Squirtle', value: 'squirtle'}
+    // ];
+    useEffect(() => {
+        fetch(' https://pokeapi.co/api/v2/pokemon?limit=100')
+            .then(response => response.json())
+            .then(dados => setListaPokemon(dados.results))
+    }, [])
+    console.log('fora')
+
+    return (
+        <View style={style.container}>
+            <View style = {style.box}>
+            <Text style={style.title}>Selecione um Pokemon</Text>
+            <Picker
+                selectedValue={pokemon}
+                style={style.picker}
+                onValueChange={(itemValue) => setPokemon(itemValue)}
+            >
+                <Picker.Item label="Selecione um Pokemon" />
+                {lista_pokemon.map((item, index) => (
+                    <Picker.Item key={index} label={item.name} value={item.url} />
+                ))}
+            </Picker>
+            {pokemon ? <Text style={style.final}>Voce selecionou {pokemon}</Text> : ''}
+            </View>
         </View>
     );
 };
